42fd22f9c38cf78068a6e6a32828e31c713c90b6
jdk <ehdrylang@naver.com>
2022-10-07 18:04:16 +0900

    feat : 에러메시지 출력하는 기능 개발

--- src/main/java/racingcar/view/Viewer.java
+++ src/main/java/racingcar/view/Viewer.java
@@ -3,6 +3,7 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Viewer {
+    private static final String ERROR_PREFIX = "[ERROR] ";
     public String inputCarNames() {
         System.out.println(InputText.CAR_NAME.getText());
         return Console.readLine();
@@ -11,5 +12,9 @@ public class Viewer {
     public String inputAttemptCount() {
         System.out.println(InputText.NUMBER_OF_ATTEMPTS.getText());
         return Console.readLine();
+    }
+
+    public void printErrorMessage(String errorMessage) {
+        System.out.println(ERROR_PREFIX + errorMessage);
     }
 }
