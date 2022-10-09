c5ca33103c31eb5ecbc75df84cf836be978d04ca
jdk <ehdrylang@naver.com>
2022-10-09 15:57:56 +0900

    feat : 각 자동차들이 몇 번 이동할 거신지 입력받았을 때, 0 이하의 이동 횟수를 입력하는 경우 예외 처리

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,17 +1,21 @@
 package racingcar.controller;
 
+import racingcar.model.AttemptCount;
 import racingcar.model.CarCollection;
 import racingcar.model.CarNames;
 import racingcar.view.Viewer;
 
 public class RacingGame {
-    private Viewer viewer;
+    private final Viewer viewer;
 
     public RacingGame() {
         this.viewer = new Viewer();
     }
     public void start() {
         createRacingCars();
+        String attemptCount = viewer.inputAttemptCount();
+        AttemptCount count = new AttemptCount(attemptCount);
+
     }
     private CarCollection createRacingCars() {
         String carNames = viewer.inputCarNames();
