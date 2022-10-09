ad0e14e58d30bf0230f702352ba049262d5462f3
jdk <ehdrylang@naver.com>
2022-10-09 17:27:49 +0900

    feat : 각 자동차가 전진하는 조건을 만드는 기능 개발
    조건에 따라 자동차를 제어하는 기능
    회차별 자동차 전진 상태를 나타내는 기능

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,25 +1,57 @@
 package racingcar.controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.model.AttemptCount;
 import racingcar.model.CarCollection;
 import racingcar.model.CarNames;
 import racingcar.view.Viewer;
 
 public class RacingGame {
+    private static final int MIN = 0;
+    private static final int MAX = 9;
     private final Viewer viewer;
+    private CarCollection racingCars;
 
     public RacingGame() {
         this.viewer = new Viewer();
     }
+
     public void start() {
-        createRacingCars();
-        String attemptCount = viewer.inputAttemptCount();
-        AttemptCount count = new AttemptCount(attemptCount);
+        racingCars = createRacingCars();
+        AttemptCount count = getAttemptCount();
+        for (int i = 0; i < count.getCount(); i++) {
+            race(racingCars.getCarCount());
+        }
 
     }
+
+    private AttemptCount getAttemptCount() {
+        String attemptCount = viewer.inputAttemptCount();
+        return new AttemptCount(attemptCount);
+    }
+
+    private void race(int carCount) {
+        for (int i = 0; i < carCount; i++) {
+            move(i);
+        }
+    }
+
+    private void move(int i) {
+        if (canMove()) {
+            racingCars.go(i);
+            return;
+        }
+        racingCars.stop(i);
+    }
+
+    private boolean canMove() {
+        return Randoms.pickNumberInRange(MIN, MAX) >= 4;
+    }
+
     private CarCollection createRacingCars() {
         String carNames = viewer.inputCarNames();
         CarNames names = new CarNames(carNames);
         return new CarCollection(names);
     }
+
 }
--- src/main/java/racingcar/model/CarCollection.java
+++ src/main/java/racingcar/model/CarCollection.java
@@ -15,4 +15,34 @@ public class CarCollection {
             cars.add(new Car(carName));
         }
     }
+    public void go(int index) {
+        if (!isValidIndex(index)) {
+            return;
+        }
+        Car car = cars.get(index);
+        car.go();
+    }
+    public void stop(int index) {
+        if (!isValidIndex(index)) {
+            return;
+        }
+        Car car = cars.get(index);
+        car.stop();
+    }
+    public int getCarCount() {
+        return this.cars.size();
+    }
+    private boolean isValidIndex(int index) {
+        return index >= 0 && index < cars.size();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Car car : this.cars) {
+            sb.append(car);
+            sb.append(System.lineSeparator());
+        }
+        return sb.toString();
+    }
 }
